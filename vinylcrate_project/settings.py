7abfbc3ad99f44ff3c29f5b99aeb2fb31690ef98
sd-powell <kf210rhd@students.codeinstitute.net>
2025-05-01 14:33:26 +0100

    add secret key to env.py and remove default in settings

--- vinylcrate_project/settings.py
+++ vinylcrate_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-j3s!dc1zkl29-pnfhjooup#8*2p)_3^v9j14#dm2%f3vr)egn+"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
