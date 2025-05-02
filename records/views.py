d64e80c2a4194d226195e646b44de4ed7f3c40e4
sd-powell <kf210rhd@students.codeinstitute.net>
2025-05-02 14:24:52 +0100

    add record_list view to show all records belonging to the user

--- records/views.py
+++ records/views.py
@@ -6,3 +6,8 @@ from .forms import RecordForm # Form to be created
 
 # Create your views here.
 
+# List View – Shows all records belonging to the logged-in user
+@login_required
+def record_list(request):
+    records = Record.objects.filter(user=request.user)
+    return render(request, 'records/record_list.html', {'records': records})
