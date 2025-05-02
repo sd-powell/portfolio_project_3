5f73e370cf529161f6259b37574e059eda8f1868
sd-powell <kf210rhd@students.codeinstitute.net>
2025-05-02 13:50:08 +0100

    create forms.py and Set up base imports for function-based record views

--- records/views.py
+++ records/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Record
+from .forms import RecordForm # Form to be created
+
 
 # Create your views here.
+
