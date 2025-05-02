cd4af4595fe7236ff62f23c1ff3fc5cb46514276
sd-powell <kf210rhd@students.codeinstitute.net>
2025-05-02 11:00:42 +0100

    register record model in admin.py

--- records/admin.py
+++ records/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Record
+
+
+@admin.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('title', 'artist', 'user', 'genre', 'year', 'bpm', 'key', 'rating')
+    list_filter = ('genre', 'rating', 'key', 'year')
+    search_fields = ('title', 'artist', 'user__username')
