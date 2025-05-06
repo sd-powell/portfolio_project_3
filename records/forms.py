be938048177afa491d7600c3ab7c932646db5aad
sd-powell <kf210rhd@students.codeinstitute.net>
2025-05-06 14:37:53 +0100

    add labels to recordform to override default field names

--- records/forms.py
+++ records/forms.py
@@ -5,4 +5,23 @@ from .models import Record
 class RecordForm(forms.ModelForm):
     class Meta:
         model = Record
-        fields = ['title', 'artist', 'year', 'genre', 'bpm', 'key', 'cover_image', 'rating']
+        fields = [
+            'title',
+            'artist',
+            'year',
+            'genre',
+            'bpm',
+            'key',
+            'cover_image',
+            'rating']
+
+        labels = {
+            'title': 'Record Title',
+            'artist': 'Artist Name',
+            'year': 'Release Year',
+            'genre': 'Genre',
+            'bpm': 'BPM (Beats Per Minute)',
+            'key': 'Musical Key (Camelot Notation)',
+            'cover_image': 'Cover Image',
+            'rating': 'Rating (1-5)',
+        }
