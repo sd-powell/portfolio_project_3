dcb4320d010e0840819f8d78b8f39c18f40b67f2
sd-powell <kf210rhd@students.codeinstitute.net>
2025-05-07 15:56:45 +0100

    add docstring to RecordsConfig

--- records/apps.py
+++ records/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class RecordsConfig(AppConfig):
+    """
+    Configuration for the 'records' app.
+
+    Sets default primary key field type and identifies the app name.
+    """
     default_auto_field = "django.db.models.BigAutoField"
     name = "records"
