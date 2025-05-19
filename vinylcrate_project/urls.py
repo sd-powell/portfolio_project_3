c1d2ffec3f4fed41c25f204aa4757d55b9f8b5da
sd-powell <kf210rhd@students.codeinstitute.net>
2025-05-19 10:08:58 +0100

    refactor URL routing to simplify login redirects and avoid duplicate paths

--- vinylcrate_project/urls.py
+++ vinylcrate_project/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
-    path("records/", include("records.urls")),
     path("", include("records.urls")),
 ]
 
