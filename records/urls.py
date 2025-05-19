5f1670edb6fef328a50d5bbcd4021588e3d19e93
sd-powell <kf210rhd@students.codeinstitute.net>
2025-05-19 09:56:47 +0100

    update app level urls with prefix to avoid clashes

--- records/urls.py
+++ records/urls.py
@@ -11,8 +11,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('records/', views.record_list, name='record_list'),
-    path('add/', views.record_create, name='record_create'),
-    path('<int:pk>/', views.record_detail, name='record_detail'),
-    path('<int:pk>/edit/', views.record_update, name='record_update'),
-    path('<int:pk>/delete/', views.record_delete, name='record_delete'),
+    path('records/add/', views.record_create, name='record_create'),
+    path('records/<int:pk>/', views.record_detail, name='record_detail'),
+    path('records/<int:pk>/edit/', views.record_update, name='record_update'),
+    path('records/<int:pk>/delete/', views.record_delete, name='record_delete'),
 ]
