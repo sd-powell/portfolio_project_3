022ddcaa03dbaa1aa86aa609934e9a302f0425fd
sd-powell <kf210rhd@students.codeinstitute.net>
2025-05-22 15:19:40 +0100

    fix url issue by moving record detail route under other routes

--- records/urls.py
+++ records/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
         name='record_collection'
     ),
     path('add/', views.record_create, name='record_create'),
-    path('<int:pk>/', views.record_detail, name='record_detail'),
     path('<int:pk>/edit/', views.record_update, name='record_update'),
     path('<int:pk>/delete/', views.record_delete, name='record_delete'),
+    path('<slug:slug>/', views.record_detail, name='record_detail'),
 ]
